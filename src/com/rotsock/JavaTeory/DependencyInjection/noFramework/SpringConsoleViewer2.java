b82d018cc176bffc35e046b1dcf9d22dc21a78a8
czx0274 <czx0274@fgarcia-PC.tgies.com>
2018-04-10 13:08:51 +0200

    Añadido módulo de Spring y Maven

--- src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringConsoleViewer2.java
+++ src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringConsoleViewer2.java
@@ -5,18 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-public class SpringConsoleViewer implements View {
+public class SpringConsoleViewer2 implements View {
+
 
 	final private Model modelProvider;
 
-	
-	
-	SpringConsoleViewer(Model modelProvider){
+	SpringConsoleViewer2(Model modelProvider){
 		this.modelProvider = modelProvider;
 	}
 	
 	@Override
 	public void displayMessage() {
-		System.out.println(modelProvider.getMessage()+"1 Spring View");
+		System.out.println(modelProvider.getMessage()+"2 Spring View");
 	}
 }
--- src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringController.java
+++ src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringController.java
@@ -1,11 +1,76 @@
 package com.rotsock.JavaTeory.DependencyInjection.noFramework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
 public class SpringController {
 	
+	/**
+	 * Defino un bean por vista, en cada uno defino una view distinta 
+	 * SpringConsoleViewer o  SpringConsoleViewer2
+	 * 
+	 * En cada bean de la vista paso un modelo en concreto, lo que hago es:
+	 * @Qualifier("springmodel1") Model springmodel1 para decir quiero como parametro de la vista el bean "springmodel1"
+	 */
+	
+	
+	@Bean(name = "viewModel")
+	public View springview(@Qualifier("springmodel1") Model springmodel1){
+		return new SpringConsoleViewer(springmodel1);
+	}
+	
+	
+	@Bean(name = "viewModel2")
+	public View springview2(@Qualifier("springmodel2") Model springmodel2){
+		return new SpringConsoleViewer2(springmodel2);
+	}
+	
+	@Bean(name = "viewModel3")
+	public View springview3(@Qualifier("springmodel2") Model springmodel2){
+		return new SpringConsoleViewer(springmodel2);
+	}
+	
+	
+	
+	
+	@Bean
+	public Model  springmodel1 (){
+		return new Model(){
+			@Override
+			public String getMessage() {
+				return "1 Bean Model:";
+			}
+			
+		};
+	}
+	
+	@Bean
+	public Model  springmodel2 (){
+		return new Model(){
+			@Override
+			public String getMessage() {
+				return " 2 Bean Model:";
+			}
+			
+		};
+	}
+	
 	public static void main (String[] args){
-		ApplicationContext cts = new  AnnotationConfigApplicationContext ("di.spring");
-		View v = cts.getBean("myView",View.class);
-		v.displayMessage();
+		ApplicationContext cts = new  AnnotationConfigApplicationContext (SpringController.class);
+		try{
+			View v1_m1 = cts.getBean("viewModel",View.class);
+			View v2_m2 = cts.getBean("viewModel2",View.class);
+			View v1_m2 = cts.getBean("viewModel3",View.class);
+			v1_m1.displayMessage();
+			v2_m2.displayMessage();
+			v1_m2.displayMessage();
+		}
+		finally{
+			((AnnotationConfigApplicationContext) cts).close();
+		}
 	}
 	
 	//tasks to do
