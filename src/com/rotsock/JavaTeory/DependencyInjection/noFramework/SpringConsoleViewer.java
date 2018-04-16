56df52678464fe1cc6d52cc83b48248dcd49a109
czx0274 <czx0274@fgarcia-PC.tgies.com>
2018-04-16 17:38:19 +0200

    Limpiado unas librerías superfluas

--- src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringConsoleViewer.java
+++ src/com/rotsock/JavaTeory/DependencyInjection/noFramework/SpringConsoleViewer.java
@@ -1,10 +1,6 @@
 package com.rotsock.JavaTeory.DependencyInjection.noFramework;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
 public class SpringConsoleViewer implements View {
 
 	final private Model modelProvider;
